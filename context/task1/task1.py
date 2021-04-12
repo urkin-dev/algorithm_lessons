2612c881d5dd19cce29336df9b3ea828037f4353
Ivan <yurkin.ivan5456@gmail.com>
2021-04-12 12:59:30 +0300

    add: Heap sort and context task1

---
+++ context/task1/task1.py
@@ -0,0 +1,17 @@
+import sys
+import random
+
+input = open('./input.txt', 'r')
+list = input.readlines()[0]
+list = list.split(' ')
+input.close()
+
+#Init
+q, v, p, n, k = int(list[0]), int(list[1]), int(list[2]), int(list[3]), int(list[4])
+a = [p]
+
+for i in range(1, n):
+    a.append((a[i-1] * q) % v)
+
+A = sorted(a)
+print(A[k-1])
