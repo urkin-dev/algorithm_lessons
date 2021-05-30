f4c08636675afb4092a35d650972e2850b51ffdc
Ivan <yurkin.ivan5456@gmail.com>
2021-05-30 21:10:49 +0300

    change: subsets to quicker solution

--- leetcode/subsets.py
+++ leetcode/subsets.py
@@ -1,13 +1,22 @@
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         arr = []
+#         self.possibleSubs(arr, [], 0, nums)
+
+#         return arr
+
+#     def possibleSubs(self, arr, path, idx, nums):
+#         arr.append(path)
+
+#         if len(path) == len(nums): return
+#         for i in range(idx, len(nums)):
+#             self.possibleSubs(arr, path + [nums[i]], i + 1, nums)
+
+# Quicker solution
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        arr = []
-        self.possibleSubs(arr, [], 0, nums)
-
-        return arr
-
-    def possibleSubs(self, arr, path, idx, nums):
-        arr.append(path)
-
-        if len(path) == len(nums): return
-        for i in range(idx, len(nums)):
-            self.possibleSubs(arr, path + [nums[i]], i + 1, nums)
+        output = [[]]
+        
+        for n in nums:
+            output += [current + [n] for current in output]
+        return output
