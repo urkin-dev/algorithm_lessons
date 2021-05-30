2adf1a9981f34355fa6786efd31d676d19f87ba2
Ivan <yurkin.ivan5456@gmail.com>
2021-05-30 16:06:18 +0300

    change: gitignore

--- sort/quicksort.py
+++ sort/quicksort.py
@@ -5,15 +5,13 @@ import random
 
 def quickSort(list):
     if (len(list) > 1):
-        base = list[0]  # Take first elemtn of the list as base value
-
         base = random.choice(list)
 
         less = [i for i in list if i < base]
         greater = [i for i in list if i > base]
         equal = [i for i in list if i == base]
 
-        return self.quickSort(less) + equal + self.quickSort(greater)
+        return quickSort(less) + equal + quickSort(greater)
     else:
         return list
 
